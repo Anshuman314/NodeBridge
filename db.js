835f912d20fc37109dbe7a6e308700d3c68679be
Anshuman <73136987+Anshuman314@users.noreply.github.com>
2024-07-17 02:04:00 +0530

    deleted

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost:27017/meanDB', (err) => {
-    if(err) throw err;
-
-    console.log('DB connection eslablished')
-})
-
-module.exports = mongoose;
